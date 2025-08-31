c69d48ffa2ab0cd8b2674c775544f273fe2cfc0b
Ruitiari <ruitiarii@gmail.com>
2025-08-31 13:43:08 +0300

    Update VarySize.kt
    added ordinal based look up
    updated rememberVarySize function use a simple for-loop
    added a binary-search based variant of the rememberVarySize function

--- vary/src/commonMain/kotlin/org/bizilabs/vary/models/VarySize.kt
+++ vary/src/commonMain/kotlin/org/bizilabs/vary/models/VarySize.kt
@@ -14,6 +14,15 @@ sealed class VarySize(val minWidth: Int) {
 
     companion object {
         val all = listOf(XS, SM, MD, LG, XL, XXL)
+        // ordinal based look up which is pre computed
+        fun VarySize.ordinal(): Int = when(this) {
+            XS -> 0
+            SM -> 1
+            MD -> 2
+            LG -> 3
+            XL -> 4
+            XXL -> 5
+        }
     }
 }
 
@@ -22,6 +31,32 @@ val LocalVarySize = compositionLocalOf { 0 }
 @Composable
 fun rememberVarySize(width: Int): VarySize {
     return remember(width) {
-        VarySize.all.last { width >= it.minWidth }
+        // find the last matching size. We eliminate use of 'last' because it has to create a listIterator
+        // behind the scenes...this is O(n), we can break it down to O(log n) using binary search
+        for (i in VarySize.all.lastIndex downTo 0){
+            val size = VarySize.all[i]
+            if(width >= size.minWidth) return@remember  size
+        }
+        VarySize.XS // this is the fallback if the predicate does not work
+    }
+}
+
+@Composable
+fun rememberVarySizeUsingBinary(width: Int): VarySize {
+    return remember(width) {
+        var left =0
+        var right = VarySize.all.lastIndex
+        var result: VarySize = VarySize.XS
+        while (left <= right){
+            val mid = (left+right)/2
+            val midSize = VarySize.all[mid]
+            if (width >= midSize.minWidth){
+                result = midSize
+                left = mid+1
+            }else{
+                right = mid-1
+            }
+        }
+        result
     }
 }
